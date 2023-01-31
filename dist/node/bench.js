fc1094f8207a98fc43e378628d2213f1771d6b64
Makarov Andrei <makarov-and@yandex-team.ru>
2023-01-31 16:34:50 +0300

    chdir param

--- dist/node/bench.js
+++ dist/node/bench.js
@@ -40,6 +40,10 @@ const debugMode = (argv.d || argv.debugMode || '0') == '1';
 const threads = Number.parseInt(argv.t || argv.threads || '10');
 const iterations = Number.parseInt(argv.i || argv.iterations || '1000');
 const percentiles = (argv.p || argv.percentiles || '0,50,95,99,99.9').split(',').map((val) => Number.parseFloat(val));
+const projectDirPath = argv.h || argv.projectDirPath;
+if (projectDirPath !== undefined) {
+    process.chdir(projectDirPath);
+}
 const resultFilePath = path.resolve(argv.r || argv.resultFilePath || './result.txt');
 if (debugMode) {
     console.log('Bench counter started in debug mode.');
